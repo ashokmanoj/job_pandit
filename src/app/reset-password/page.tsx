2eb31b1a95c15092025e30fc3ef74d805ee271d8
Dev <devsg777@gmail.com>
2024-05-30 20:22:21 +0530

    Complete Candidate Details page and account setting and candidate Dashboard

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -5,10 +5,11 @@ const page = () => {
   return (
     <div>
         <>
-        <section className="registration-section position-relative pt-100 lg-pt-80 pb-150 lg-pb-80">
+      <section className="registration-section position-relative pt-100 lg-pt-80 pb-150 lg-pb-80">
       <div className="container">
         <div className="user-data-form">
-        <div className="form-wrapper m-auto"><ResetPassword />
+        <div className="form-wrapper m-auto">
+          <ResetPassword />
         </div>
         </div>
       </div>
